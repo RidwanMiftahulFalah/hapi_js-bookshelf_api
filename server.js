20a87f2244318d3070f668334c90559d839d03dc
RidwanMiftahulFalah <miftahridwan1101@gmail.com>
2022-07-25 09:39:16 +0700

    Add add book functionality

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-console.log('Hello World!');
+const hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = (async () => {
+  const server = hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+});
+
+init();
